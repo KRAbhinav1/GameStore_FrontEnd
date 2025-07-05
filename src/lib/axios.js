663a5f25973e54baa416b2634d5ce2fe8319083c
KRAbhinav1 <abhinavkr064@gmail.com>
2025-07-05 11:13:01 +0530

    bug fixed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:4000",
+    baseURL:"https://gamestore-backend-u2p3.onrender.com",
     withCredentials: true
 })
